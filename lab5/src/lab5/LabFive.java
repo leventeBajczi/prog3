2be6783b852a78b6d86f4901fa3b6d4679a7f22b
Levente Bajczi <bajczilevi@gmail.com>
2018-10-07 16:53:29 +0200

    aufgabe1 done

--- lab5/src/lab5/LabFive.java
+++ lab5/src/lab5/LabFive.java
@@ -1,8 +1,15 @@
 package lab5;
 
+import lab5.aufgabe1.Producer;
+
 public class LabFive {
 
     public static void main(String[] args) {
-
+        Producer p = new Producer("demo");
+        try{
+            p.go();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
